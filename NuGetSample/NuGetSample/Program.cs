f52bee05b444e27ccb660b4fd672b0a4732d9057
Alex Ellis <AlexEllis2@gmail.com>
2015-11-16 17:04:27 +0000

    Initial

---
+++ NuGetSample/NuGetSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Usage: calc 5 5");
+                return;
+            }
+
+            int val = new Calc().Multiply(int.Parse(args[0]), int.Parse(args[1]));
+            Console.WriteLine("Sum: {0}.", val);
+        }
+    }
+}
